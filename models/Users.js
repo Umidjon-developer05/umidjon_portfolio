caa1268096c701d2d05bfcb407513f8673d818b7
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-29 23:47:37 +0500

    first commitaaaa

--- models/Users.js
+++
@@ -1,9 +0,0 @@
-import {model, models, Schema} from "mongoose";
-
-const UsersSchema = new Schema({
-  name:String,
-  email:String,
-});
-
-const Users = models?.Users || model('Users', UsersSchema);
-export default Users
