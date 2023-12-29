907fb7f192bedeb45032c65c913cb8563a492a9b
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-29 23:45:19 +0500

    first commitaaa

--- app/SingIn/page.jsx
+++ app/SingIn/page.jsx
@@ -20,50 +20,11 @@ const Register = () => {
     route?.push("/");
   }
 
-  const handleSubmit = async (e) => {
-    e.preventDefault();
-    const name = e.target[0].value;
-    const email = e.target[1].value;
-
-    try {
-     if (name === process.env.name && email === process.env.email) {
-      await fetch("http://localhost:3000/api/singin", {
-        method: "POST",
-        headers: {
-          "Content-Type": "application/json",
-        },
-        body: JSON.stringify({
-          name,
-          email,
-        }),
-      });
-     }
-    } catch (err) {
-      setError("Something went wrong!");
-      console.error(err);
-    }
-  };
+ 
 
   return (
     <div className={styles.container}>
-      <form onSubmit={handleSubmit} className={styles.form}>
-        <input
-          type="text"
-          placeholder="Name"
-          name="name" // Add name attribute for capturing input value
-          required
-          className={styles.input}
-        />
-        <input
-          type="email"
-          placeholder="Email"
-          name="email" // Add name attribute for capturing input value
-          required
-          className={styles.input}
-        />
-        <button type="submit" className={styles.button}>Register</button>
-        {error && <p>{error}</p>}
-      </form>     
+      
       <button
         onClick={() => {
           signIn("google");
