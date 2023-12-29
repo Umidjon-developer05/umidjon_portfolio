67c4037d1cfd034e9a4874331cf6ee4ef3f26567
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-30 00:05:31 +0500

    first commitaaaasa

--- app/api/auth/[...nextauth]/route.js
+++ app/api/auth/[...nextauth]/route.js
@@ -1,14 +1,63 @@
 import NextAuth from "next-auth";
+import GithubProvider from "next-auth/providers/github";
 import GoogleProvider from "next-auth/providers/google";
+import CredentialsProvider from "next-auth/providers/credentials";
+import Users from "@/models/Users";
+import connect from "@/libs/db";
+import bcrypt from "bcryptjs";
 
 const handler = NextAuth({
   providers: [
+    CredentialsProvider({
+      id: "credentials",
+      name: "Credentials",
+      async authorize(credentials) {
+        // Check if the user exists.
+        await connect()
+
+        try {
+          const user = await Users.findOne({
+            email: credentials.email,
+            IsAdmin,
+          });
+          
+          if (user) {
+            const isPasswordCorrect = await bcrypt.compare(
+              credentials.password,
+              user.password
+              );
+              
+              if (isPasswordCorrect) {
+                // Check if the provided email matches a specific email
+                const isAdminEmail = credentials.email === process.env.email;
+                
+                // Set isAdmin based on the email condition
+              const isAdmin = isAdminEmail ? true : false;
+
+              // Return user object with modified email and isAdmin
+              return {
+                ...user,
+                email: isAdminEmail ? credentials.email : user.email, // If matches, set provided email, else use existing user email
+                isAdmin: isAdmin,
+              };
+            } else {
+              throw new Error("Wrong Credentials!");
+            }
+          } else {
+            throw new Error("User not found!");
+          }
+        } catch (err) {
+          throw new Error(err);
+        }
+      },
+    }),
+    
     GoogleProvider({
       clientId: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
     }),
   ],
-  secret: process.env.NEXTAUTH_SECRET, // Define your secret here
+  secret: process.env.NEXTAUTH_SECRET,
 });
 
-export default handler;
+export { handler as GET, handler as POST };
---
+++ components/Loading.jsx
@@ -0,0 +1,10 @@
+
+import { BoxLoading } from 'react-loadingg';
+
+const Loading = () => {
+  return(
+    <BoxLoading />
+  )
+};
+
+export default Loading;
