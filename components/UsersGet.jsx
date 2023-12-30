ac93ed7022321f3214f28eed6a1b71373d361a9e
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-30 12:42:21 +0500

    Your commit message

--- components/UsersGet.jsx
+++ components/UsersGet.jsx
@@ -88,8 +88,8 @@ const UsersGet = () => {
           }}
         >
           <div>
-            <h2 className="font-bold text-2xl">{t.title}</h2>
-            <div>{t.description}</div>
+            <h2 className="font-bold text-2xl" style={{color: clickedButtonId === t._id && desc === 'ish bajarildi😁' ?'#fff':'#222'}}>{t.title}</h2>
+            <div  style={{color: clickedButtonId === t._id && desc === 'ish bajarildi😁' ?'#fff':'#222'}}>{t.description}</div>
           </div>
 
           <div className="flex gap-2 align-items-center">
