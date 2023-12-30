1b862458320a1c160db6224bbb4733420ef5eee8
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2023-12-30 11:18:12 +0500

    Your commit message

--- components/UsersGet.jsx
+++ components/UsersGet.jsx
@@ -101,11 +101,11 @@ const UsersGet = () => {
           style={{
             borderRadius: "20px",
             backgroundColor:
-              clickedButtonId === t._id && title === 'ish bajarildi😄'
-                ? 'green'
-                : clickedButtonId === t._id && title === 'ish bajarilmadi😔'
-                ? 'red'
-                : 'initial',
+            clickedButtonId === t._id && title === 'ish bajarildi😄'
+              ? 'green'
+              : clickedButtonId === t._id && title === 'ish bajarilmadi😔'
+              ? 'red'
+              : 'initial',
           }}
         >
           <div>
