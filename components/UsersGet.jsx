7022bcd1e1df786bac576c43d092e895dc07ab5f
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-01-06 17:14:13 +0500

    first commit

--- components/UsersGet.jsx
+++ components/UsersGet.jsx
@@ -128,12 +128,12 @@
               height:"300px"
             }}
           >
-            <div style={{display:"flex",flexDirection:"column",overflow: "auto",height:"200px"}} >
+            <div style={{display:"flex",flexDirection:"column",overflow: "auto",height:"220px"}} >
               <h2 className="font-bold text-2xl" style={{color: clickedButtonId === t._id && desc === 'ish bajarildi😁' ?'#222':'#222'}}>{t.title}</h2>
               <div style={{color: clickedButtonId === t._id && desc === 'ish bajarildi😁' ?'#222':'#222'}}>{t.description}</div>
             </div>
            
-            <div className="flex gap-2 justify-between " style={{alignItems:"end",height:"100%"}}>
+            <div className="flex gap-2 justify-between " style={{alignItems:"end"}}>
               <div style={{color: clickedButtonId === t._id && desc === 'ish bajarildi😁' ?'#222':'#222'}}>{t.time}</div>
              <div>
              <button
