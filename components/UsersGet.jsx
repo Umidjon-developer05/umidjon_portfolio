108a8a99347390590f86d44614adcc90ba86b48e
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-01-06 16:07:10 +0500

    first commit

--- components/UsersGet.jsx
+++ components/UsersGet.jsx
@@ -121,7 +121,7 @@
         {topics.topics?.map((t) => (
           <div
             key={t._id}
-            className="p-4 border border-slate-300 my-3 flex justify-between gap-5 m-3 items-center"
+            className="card p-4 border border-slate-300 my-3 flex justify-between gap-5 m-3 items-center"
             style={{
               borderRadius: "20px",
               
