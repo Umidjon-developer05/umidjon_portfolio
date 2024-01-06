0ebe3ada1d86592444872a39e4a1d5beea9dbbff
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-01-06 11:05:49 +0500

    first commit

--- app/api/topics/route.js
+++ app/api/topics/route.js
@@ -3,9 +3,9 @@ import Topic from "@/models/topic";
 import { NextResponse } from "next/server";
 
 export async function POST(request) {
-  const { title, description ,time} = await request.json();
+  const { title, description ,time,startTime,endTime} = await request.json();
   await connectMongoDB();
-  await Topic.create({ title, description ,time});
+  await Topic.create({ title, description ,time,startTime,endTime});
   return NextResponse.json({ message: "Topic Created" }, { status: 201 });
 }
 
