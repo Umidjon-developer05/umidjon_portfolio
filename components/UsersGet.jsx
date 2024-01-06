c52290e35e52877158422c984355ee4bdcb7fc42
Umidjon <127615615+123456789098765432111@users.noreply.github.com>
2024-01-06 12:04:27 +0500

    first commit

--- components/UsersGet.jsx
+++ components/UsersGet.jsx
@@ -107,9 +107,9 @@ const UsersGet = () => {
 
   // Format seconds to display as HH:MM:SS
   const formatTime = (seconds) => {
-    const hours = Math.floor(seconds / 3600);
-    const minutes = Math.floor((seconds % 3600) / 60);
-    const remainingSeconds = seconds % 60;
+    // const hours = Math.floor(seconds / 3600);
+    // const minutes = Math.floor((seconds % 3600) / 60);
+    const remainingSeconds = seconds ;
 
     return `${String(hours).padStart(2, '0')}:${String(minutes).padStart(2, '0')}:${String(remainingSeconds).padStart(2, '0')}`;
   };
