b02cea548bfaa561b84c899cfc61a61343d87582
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-04-13 22:50:35 +0500

    zindex

--- app/Main/_components/HeroContent.jsx
+++ app/Main/_components/HeroContent.jsx
@@ -15,7 +15,7 @@ const HeroContent = () => {
     <motion.div
       initial="hidden"
       animate="visible"
-      className="flex items-center justify-center px-20 mt-10 z-[20]"
+      className="flex items-center justify-between px-20 mt-10  mb-20"
     >
       <div className="h-full flex flex-col gap-5 justify-center m-auto text-start">
         <motion.div
