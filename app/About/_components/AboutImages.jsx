46c02405bc1dacad369cbb2da22135ddd77fe3d6
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-08-06 14:03:41 +0500

    about

--- app/About/_components/AboutImages.jsx
+++ app/About/_components/AboutImages.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const AboutImages = () => {
   return (
-    <div  className=' border w-[450px] h-[400px] rounded-lg cursor-pointer transition-transform duration-100 transform hover:scale-90' style={{ backgroundImage: 'url(/CoderUser.webp)', backgroundSize:"cover",backgroundRepeat:"no-repeat"}}>
+    <div  className=' border w-[350px] h-[300px] rounded-lg cursor-pointer transition-transform duration-100 transform hover:scale-90' style={{ backgroundImage: 'url(/CoderUser.webp)', backgroundSize:"cover",backgroundRepeat:"no-repeat"}}>
         
     </div>
   )
--- app/About/_components/AboutText.jsx
+++ app/About/_components/AboutText.jsx
@@ -4,7 +4,7 @@ import React from 'react'
 const AboutText = () => {
   return (
     <div className='flex flex-col gap-10  lg:max-w-screen-lg '>
-    <h1 className='font-serif text-2xl sm:text-4xl lg:text-3xl xl:text-6xl '>
+    <h1 className='font-serif text-xl sm:text-4xl lg:text-3xl xl:text-6xl '>
       G'afforov Umidjon
     </h1>
     <h1 className='text-base sm:text-lg sm:max-w-[720px]  lg:text-xl  font-light leading-6'>
