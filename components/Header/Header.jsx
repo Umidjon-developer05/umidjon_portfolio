769dab121698713a95ea94d0a3f23e6060d4f8ae
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-08-06 16:21:18 +0500

    asdasd

--- components/Header/Header.jsx
+++ components/Header/Header.jsx
@@ -13,9 +13,9 @@ const Header = () => {
           <div className="border mt-5 w-36 cursor-pointer h-36 hover:animate-pulse m-auto flex justify-center items-center" style={{ borderRadius: "50%" }}>
             <Image 
               src={'/umidjon.jpg'}
-              width={"200px"}
+              width={"250px"}
               alt='assa'
-              style={{ borderRadius: "50%", height: "200px" }}
+              style={{ borderRadius: "50%", height: "100px" }}
             />
           </div>
           <h1 className='font-semibold text-[22px] text-center mt-2'>Umidjon G'afforov</h1>
