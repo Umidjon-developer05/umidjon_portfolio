7ca4fdffff2b9ab26cd98a371680fed38cd0e99b
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-08-06 13:55:07 +0500

    main

--- app/Main/page.jsx
+++ app/Main/page.jsx
@@ -4,14 +4,7 @@ import HeroContent from "./_components/HeroContent";
 const Main = () => {
   return (
     <div className="relative flex flex-col h-full w-full " id="about-me">
-      <video
-        autoPlay
-        muted
-        loop
-        className="rotate-180 absolute   h-full w-full left-0  object-cover "
-      >
-        <source src="/blackhole.webm" type="video/webm" />
-      </video>
+  
       <HeroContent />
     </div>
   );
