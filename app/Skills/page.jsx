c9490b505a19fe97462c0f49eee9ed6dab827bbd
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2024-08-06 15:16:58 +0500

    Skills

--- app/Skills/page.jsx
+++ app/Skills/page.jsx
@@ -19,7 +19,7 @@ const Skills = () => {
     <h1 className='text-[30px] '  data-aos="fade-right">Skills</h1>
     <div className='bg-blue-700 w-full h-1 rounded-2xl' data-aos="zoom-in">
       </div>
-      <div className='flex flex-col flex-wrap items-center'  data-aos="fade-up">
+      <div className='flex flex-col flex-wrap items-center w-full '  data-aos="fade-up">
           <div className='flex justify-between w-full z-50'>
                 <button className={`' border w-[50%] transition-all  h-16 ${Lang ? 'bg-slate-500 border-b-red-600  border-spacing-5 ': ''} '`} onClick={()=>Technologies('Frontend')}>Frontend</button>
                 <button className={`' border w-[50%] transition-all  h-16 ${!Lang ? 'bg-slate-500 border-b-red-600 border-spacing-5 ': ''} '`} onClick={()=>Technologies('Backend')}>Backend</button>
