60952cd15963c9755500ad9d0068f9f58e6e6436
Umidjon <127615615+Umidjon-developer05@users.noreply.github.com>
2025-03-05 17:08:54 +0500

    Update layout.js

--- app/layout.js
+++ app/layout.js
@@ -12,7 +12,29 @@ import Header from "@/components/Header/Header";
 import About from "./About/page";
 import { Toaster } from "@/components/ui/toaster"
 import StarsCanvas from "@/components/StarBackground";
+export const metadata: Metadata = {
+  metadataBase: new URL("https://umidjon.store"),
+  title: "Umidjon portfolio",
+  description:
+    "Umidjon Praktikum Next.js dasturlash kurslari, amaliyotlar, startup loyihalar va asosiysi sifatli ta'limdir.",
+  authors: [
+    { name: "Umidjon Gafforov ", url: "https://umidjon.store" },
+  ],
+  icons: { icon: "/logo.svg" },
+  openGraph: {
+    title: "Umidjon praktikum | Dasturlash kurslari",
+    description:
+      "Umidjon Praktikum Next.js dasturlash kurslari, amaliyotlar, startup loyihalar va asosiysi sifatli ta'limdir.",
+    type: "website",
+    url: "https://umidjon.store",
+    locale: "uz_UZ",
+    images: "https://0evi7lr5v8.ufs.sh/f/LN78d9BJu8QkU2FhAc05evHGXpqZ4SA9ilT8VdO3PBtIKJxk",
+    countryName: "Uzbekistan",
+    siteName: "Umidjon",
+    emails: "umidjongafforov175@gmail.com",
+  },
 
+};
 export default function RootLayout({ children }) {
   useEffect(() => {
     // Ensure this code runs only on the client side
@@ -25,11 +47,7 @@ export default function RootLayout({ children }) {
 
   return (
     <html lang="en">
-       <link
-        rel="icon"
-        href="/favicon.svg"
-        type="image/x-icon"
-      />
+      
       <body className={inter.className}>
         <AuthProvider>
           <div className="max-w-10xl mx-auto ">
